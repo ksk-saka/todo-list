0728e2ac698653592cf2a412de46295bdd1a89ae
Keisuke Sakamoto <ksakamoto@interlink.ne.jp>
2015-07-01 11:44:38 +0900

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name todoListApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the todoListApp
+ */
+angular.module('todoListApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
